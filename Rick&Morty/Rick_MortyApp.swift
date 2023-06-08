712f39b7dfb61db895cf92a52a5b7a5133d7af08
EREN <erteneren93@gmail.com>
2023-06-08 10:39:36 +0100

    Initial Commit

---
+++ Rick&Morty/Rick_MortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_MortyApp.swift
+//  Rick&Morty
+//
+//  Created by Eren Erten on 8.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct Rick_MortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
